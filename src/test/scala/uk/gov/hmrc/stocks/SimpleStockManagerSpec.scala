0f790763e2217187a0e6046d1c8e2908558a24db
rgallet <rgallet@grumlimited.co.uk>
2014-10-06 21:36:44 +0100

    Tests for apple and orange and non existent products

--- src/test/scala/uk/gov/hmrc/stocks/SimpleStockManagerSpec.scala
+++ src/test/scala/uk/gov/hmrc/stocks/SimpleStockManagerSpec.scala
@@ -19,5 +19,41 @@ class SimpleStockManagerSpec extends FeatureSpec with GivenWhenThen with Matcher
       Then("it should have found an apple product")
       exists shouldBe true
     }
+
+    scenario("Testing AppLE is available") {
+
+      Given("a simple stock manager")
+      val manager = new SimpleStockManager
+
+      When("testing for apple")
+      val exists = manager.isProductAvailable("AppLE")
+
+      Then("it should have found an apple product")
+      exists shouldBe true
+    }
+
+    scenario("Testing OranGe is available") {
+
+      Given("a simple stock manager")
+      val manager = new SimpleStockManager
+
+      When("testing for OranGe")
+      val exists = manager.isProductAvailable("OranGe")
+
+      Then("it should have found an orange product")
+      exists shouldBe true
+    }
+  }
+
+  scenario("Testing random product") {
+
+    Given("a simple stock manager")
+    val manager = new SimpleStockManager
+
+    When("testing for random")
+    val exists = manager.isProductAvailable("me here right now")
+
+    Then("it should not have found any product")
+    exists shouldBe false
   }
 }
