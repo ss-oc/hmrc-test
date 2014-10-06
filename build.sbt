cb1acb8480ba251f582072d9205e18485a44c58a
rgallet <rgallet@grumlimited.co.uk>
2014-10-07 00:11:09 +0100

    Added mockito

--- build.sbt
+++ build.sbt
@@ -6,4 +6,6 @@ scalaVersion := "2.11.1"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
 
+libraryDependencies += "org.mockito" % "mockito-core" % "1.9.5" % "test"
+
 mainClass := Some("uk.gov.hmrc.cli.Launcher")
--- src/test/scala/uk/gov/hmrc/cli/CommandLineParserSpec.scala
+++ src/test/scala/uk/gov/hmrc/cli/CommandLineParserSpec.scala
@@ -1,6 +1,10 @@
 package uk.gov.hmrc.cli
 
-import org.scalatest.{Matchers, GivenWhenThen, FeatureSpec}
+import org.mockito
+import org.mockito.Mockito
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
+import uk.gov.hmrc.products.Apple
 import uk.gov.hmrc.stocks.SimpleStockManager
 
 /**
@@ -8,7 +12,7 @@ import uk.gov.hmrc.stocks.SimpleStockManager
  *
  * Inspired from https://github.com/grumlimited/timeline
  */
-class CommandLineParserSpec extends FeatureSpec with GivenWhenThen with Matchers {
+class CommandLineParserSpec extends FeatureSpec with GivenWhenThen with Matchers with MockitoSugar {
   feature("CLI parsing") {
     scenario("Comma-separated list of products") {
 
@@ -36,8 +40,13 @@ class CommandLineParserSpec extends FeatureSpec with GivenWhenThen with Matchers
     Given("A CSV with invalid values")
     val line = "Apple, invalid1, 2invalid, inva3lid"
 
+    And("A manager")
+    val manager = mock[SimpleStockManager]
+    Mockito.when(manager.getProduct(mockito.Matchers.anyString()))
+      .thenReturn(new Apple)
+      .thenThrow(new IllegalArgumentException)
+
     And("A parser")
-    val manager = new SimpleStockManager
     val parser = new CommandLineParser(manager)
 
     Then("It should parse")
