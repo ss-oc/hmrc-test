ffeb750dd32f063b7c597d9d041525e679fc31c2
rgallet <rgallet@grumlimited.co.uk>
2014-10-06 22:01:12 +0100

    Command line parser

--- src/main/scala/uk/gov/hmrc/cli/CommandLineParser.scala
+++ src/main/scala/uk/gov/hmrc/cli/CommandLineParser.scala
@@ -3,12 +3,21 @@ package uk.gov.hmrc.cli
 import uk.gov.hmrc.products.Product
 import uk.gov.hmrc.stocks.StockManager
 
+import scala.util.{Failure, Success, Try}
+
 /**
  * User: rgallet
  * Date: 06/10/14 21:52
  */
 class CommandLineParser(stockManager: StockManager) {
   def parse(csv: String): List[Product] = {
-    throw new NoSuchMethodError()
+    csv.split(",").foldLeft(List[Product]()) { (products, productName) =>
+      Try(stockManager.getProduct(productName.trim.toLowerCase)) match {
+        case Success(v) => v :: products
+        case Failure(e) =>
+          println(s"Could not parse product [$productName]. Sorry, we only sell apples and oranges.")
+          products
+      }
+    }
   }
 }
